bda6fbf695a73126ed9530b788ab733b7347a466
tjdgns8047 <tjdgns8047@naver.com>
2017-07-24 12:48:56 +0900

    데이터 받기 진행중

--- src/containers/RequestContainer.js
+++ src/containers/RequestContainer.js
@@ -1,13 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-// import request from 'request';
-
-var request = require('request');
-var url = 'http://apis.data.go.kr/6260000/BusanShowInfoService/getShowDetailsInfo';
-var queryParams = '?' + encodeURIComponent('ServiceKey') + '=rXndeyRMn%2Fa9LoxgQcPeYtKWg2fgH34dq1pBmOJSW0UZfgR1BnNBxsyY1I6Ehif2yhPKhbjuOOe0222xIyQshA%3D%3D'; /* Service Key*/
-queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('1'); /* 한 페이지 결과 수 */
-queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent(`1`); /* 페이지 번호 */
-queryParams += '&' + encodeURIComponent('_returnType') + '=' + encodeURIComponent('json'); /* 페이지 번호 */
+import request from 'request';
+import url from '../config/url';
 
 class RequestContainer extends Component {
     constructor(props) {
@@ -16,25 +10,12 @@ class RequestContainer extends Component {
             data: ''
         };
     }
-    // _handleData = () => {
-    //     return request ({
-    //         url: url + queryParams,
-    //         method: 'GET'
-    //     }, (err, res, body)=>{
-    //         console.log(body);
-    //     })
-    // }
-    
 
     componentDidMount() {
-        request({
-            url: url + queryParams,
-            method: 'GET'
-        }, function (error, response, body) {
-            //console.log('Status', response.statusCode);
-            //console.log('Headers', JSON.stringify(response.headers));
-            console.log('Reponse received', JSON.parse(body).list);
-        });
+        request({url: url, method: 'GET'}, 
+        (error, response, body) => 
+        // { console.log('Reponse received', JSON.parse(body).list); });
+        {  this.state.data = JSON.parse(body).list; });
     }
 
     render() {
